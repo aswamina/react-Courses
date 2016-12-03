8cfbd468522fcdb7519754f462fa355240e8b64d
Anand Swaminathan <aswamina@gmail.com>
2016-12-02 18:06:27 -0600

    Added defaulf path for parent level route . Added IndexRoute for Home page

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,14 +3,15 @@
 var React = require('react');
 var Router = require('react-router').Router;
 var Route = require('react-router').Route;
+var IndexRoute = require('react-router').IndexRoute;
 var App = require('./components/app.jsx');
 var Home = require('./components/homePage.jsx');
 var About = require('./components/about/aboutPage.jsx');
 var Authors = require('./components/authors/authorPage.jsx');
 
 var routes = (
-    <Route component={App}>
-        <Route path="/" component={Home} />
+    <Route path="/" component={App}>
+        <IndexRoute component={Home} />
         <Route path="authors" component={Authors} />
         <Route path="about" component={About} />
     </Route>
