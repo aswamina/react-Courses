c2c957fdcb7962d6aaabee69580c217bfe6f5313
Anand Swaminathan <aswamina@gmail.com>
2016-12-21 10:54:41 -0600

    removed use of react-select npm package. using standard HTML select tag and created options via function createOptionRow

--- src/components/common/selectInput.jsx
+++ src/components/common/selectInput.jsx
@@ -1,16 +1,19 @@
 "use strict";
 
 var React = require('react');
-var Select = require('react-select');
 
 var selectInput = React.createClass({
     propTypes: {
         name: React.PropTypes.string.isRequired,
         onChange: React.PropTypes.func.isRequired,
-        options: React.PropTypes.array,
         error: React.PropTypes.string
     },
     render: function() {
+        var createOptionRow = function(author) {
+            return (
+                <option key={author.id} label={author.id}>{author.firstName + " " + author.lastName}</option>
+            );
+        };
         var wrapperClass = 'form-group';
         if (this.props.errors && this.props.errors.length > 0) {
             wrapperClass += " " + 'has-error';
@@ -19,11 +22,10 @@ var selectInput = React.createClass({
             <div className={wrapperClass}>
                <label htmlFor={this.props.name}>{this.props.label}</label>
                 <div className="field">
-                    <Select
-                        name={this.props.name}
-                        options={this.props.options}
-                        onChange={this.props.onChange}
-                    />
+                    <select className="form-control" name={this.props.name} onChange={this.props.onChange} defaultValue="">
+                        <option value="" disabled>Select...</option>
+                        {this.props.authors.map(createOptionRow, this)}
+                    </select>
                 </div>
                 <div className="input">{this.props.errors}</div>
             </div>
